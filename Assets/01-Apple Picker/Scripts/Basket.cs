66853c19458e5e2beeed7e5952347800db03dbe5
rayxiao9 <77815877+rayxiao9@users.noreply.github.com>
2021-02-03 16:40:25 -0500

    Added score when Apple caught

--- Assets/01-Apple Picker/Scripts/Basket.cs
+++ Assets/01-Apple Picker/Scripts/Basket.cs
@@ -1,10 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
-    void Update()
+    [Header("Set Dynamically")]
+    public Text scoreGT;   
+        
+    void Start(){
+        // Find a reference to the ScoreCounter GameObject
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+
+        // Get the Text Component of that GameObject
+        scoreGT = scoreGO.GetComponent<Text>();
+
+        // Set the starting number of points to 0
+        scoreGT.text = "0";
+    }
+
+        void Update()
     {
         // Get the current screen position of the mouse from Input
         Vector3 mousePos2D = Input.mousePosition;
@@ -26,6 +41,13 @@ public class Basket : MonoBehaviour
         GameObject collidedWith = coll.gameObject;       
         if (collidedWith.tag == "Apple") {
             Destroy(collidedWith);
+            int score = int.Parse(scoreGT.text);
+
+            // Add points for catching the apple
+            score += 100;
+
+            // Convert the score back to a string and display it
+            scoreGT.text = score.ToString();
         }
     }
 }
