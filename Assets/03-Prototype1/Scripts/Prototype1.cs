f33c23d221abbfa1d5c74d6784af3c964834bcc7
rayxiao9 <77815877+rayxiao9@users.noreply.github.com>
2021-02-17 18:59:08 -0500

    Added Game Over and restarting the game

--- Assets/03-Prototype1/Scripts/Prototype1.cs
+++ Assets/03-Prototype1/Scripts/Prototype1.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Prototype1 : MonoBehaviour
 {
     static private Prototype1 S;
     [Header("Set in Inspector")]
     public int numShots = 10;
-
     public Text shotsGT;
+    public GameObject GameOverText;
+
+    private float timer = 5f;
+
+    private bool gameOver = false;
 
     // Start is called before the first frame update
     void Start()
@@ -26,9 +31,27 @@ public class Prototype1 : MonoBehaviour
     void Update()
     {
         shotsGT.text = "Shots Left: " + numShots;
+        if (gameOver) {
+            timer -= Time.deltaTime;
+            S.GameOver();
+        }
     }
 
     public static void ShotFired() {
         S.numShots--;
+
+        if (S.numShots == 0) {
+            Destroy(GameObject.FindWithTag("Slingshot"));
+            S.GameOver();
+            // SceneManager.LoadScene("Main-Prototype 1");
+        }
+    }
+
+    void GameOver() {
+        gameOver = true;
+            GameOverText.SetActive(true);
+        if (timer <= 0) {
+            SceneManager.LoadScene("Main-Prototype 1");
+        }
     }
 }
